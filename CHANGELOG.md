d3426e4533e021e9ddb520cfa721bb7a2432f876
Bram Swenson <bram@craniumisajar.com>
2016-09-08 11:15:52 -0500

    v0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
+
+# 0.3.0
+
+  * Update metadata versionless and vagrantfile
+
 # 0.2.0
 
-Update dependency versions
+  * Update dependency versions
 
 # 0.1.0
 
-Initial release of simple-nat-cookbook
+  * Initial release of simple-nat-cookbook
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tech-crew@dailykos.com'
 license          'All rights reserved'
 description      'Installs/Configures simple-nat-cookbook'
 long_description 'Installs/Configures simple-nat-cookbook'
-version          '0.2.0'
+version          '0.3.0'
 
 depends 'iptables-ng'
 depends 'sysctl'
